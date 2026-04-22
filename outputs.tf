936d2a7b56703958d8ef9fb69011497d6c3b3622
Sigahnou Tamaboarmand <sigahnouarmand@gmail.com>
2026-04-22 12:25:28 +0200

    Add S3 bucket for WordPress storage

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "website_url" {
   description = "Access your WordPress site here"
   value       = "http://${aws_lb.wp_alb.dns_name}"
 }
+
+output "storage_bucket_name" {
+  description = "S3 bucket name for WordPress storage"
+  value       = aws_s3_bucket.wordpress_storage.bucket
+}
