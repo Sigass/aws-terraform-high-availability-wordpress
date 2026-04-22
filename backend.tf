28a678ae1df0689b7a160d7f2170c5a129b4bd3e
Sigahnou Tamaboarmand <sigahnouarmand@gmail.com>
2026-04-22 12:09:08 +0200

    delete s3

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "wordpress-terraform-state-2026" # Le nom de ton bucket S3 créé à l'étape 1
-    key            = "dev/terraform.tfstate"      # Chemin du fichier dans le bucket
-    region         = "us-west-2"                  # Ta région
-    dynamodb_table = "terraform-lock"             # Le nom de ta table DynamoDB créée à l'étape 2
-    encrypt        = true                         # Chiffrement du fichier au repos
-  }
-}
