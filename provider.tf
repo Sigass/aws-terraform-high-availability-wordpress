e86d8e76820e1775b0f865211159852edaf18e6a
Sigahnou Tamaboarmand <sigahnouarmand@gmail.com>
2026-04-23 20:12:27 +0200

    adding region

--- provider.tf
+++ provider.tf
@@ -1,13 +1,5 @@
 # Configure Terraform and the AWS provider.
 terraform {
-  cloud {
-    organization = "sigass"
-
-    workspaces {
-      name = "aws-terraform-high-availability-wordpress"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
